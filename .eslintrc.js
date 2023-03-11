38f2b814ab869b4c858b94a1dd40d07c12660895
María Bracho <mariabrachovillanueva@gmail.com>
2023-03-10 23:13:33 -0400

    feat: :sparkles: Add project config
    Add: husky, eslint, prettier, tsconfig, editorconfig and lint-staged

---
+++ .eslintrc.js
@@ -0,0 +1,20 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    node: true,
+    jest: true
+  },
+  parser: "@typescript-eslint/parser",
+  extends: ["standard-with-typescript", "prettier"],
+  overrides: [],
+  parserOptions: {
+    ecmaVersion: "latest",
+    sourceType: "module",
+    tsconfigRootDir: __dirname,
+    project: "./tsconfig.json"
+  },
+  rules: {
+    "no-console": "off"
+  }
+};
